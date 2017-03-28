167fa99ef846b96023dc4c6b60bf1051dffea7d4
Damien Castelltort <dcastelltort@gmail.com>
2017-03-28 07:11:15 +0200

    feat(UserController): added findByName route with proper Json response

--- app/controllers/UserController.scala
+++ app/controllers/UserController.scala
@@ -3,9 +3,12 @@ package controllers
 import javax.inject._
 
 import akka.actor.ActorSystem
-import models.{Feed, JsonFormats, User}
+//import models.{Feed, JsonFormats, User}
 import play.api.Logger
+//import play.api.libs.json
+import play.api.libs.json._
 import play.api.mvc.{Action, Controller}
+import reactivemongo.api.Cursor
 
 import scala.concurrent.Future
 //import play.modules.reactivemongo.json.collection.JSONCollection
@@ -42,5 +45,14 @@ extends Controller with MongoController with ReactiveMongoComponents {
           Created
       }
     }.getOrElse(Future.successful(BadRequest("invalid json")))
+  }
+
+  def findByName(lastName: String) = Action.async {
+    val cursor : Cursor[User] = collection.find(Json.obj("lastName" -> lastName)).sort(Json.obj("created" -> -1)).cursor[User]
+    val futureUserList : Future[List[User]] = cursor.collect[List]()
+
+    futureUserList.map {
+      persons => Ok(Json.toJson(persons))
+    }
   }
 }
